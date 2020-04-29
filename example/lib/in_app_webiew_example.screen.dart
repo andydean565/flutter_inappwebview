29f7cbca7d006c3adf49c7e71bffa327492cb579
andy <andrewdean565@gmail.com>
2020-04-29 15:35:54 +0100

    changing names

--- example/lib/in_app_webiew_example.screen.dart
+++ example/lib/in_app_webiew_example.screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_polywebview/flutter_inappwebview.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
 import 'main.dart';
 
